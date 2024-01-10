0448932f148741f73f0148202e0af2a1d638c03b
Fabi <75372147+irgendwielost@users.noreply.github.com>
2024-01-10 15:06:15 +0100

    Added presence

--- src/Events/Ready.js
+++ src/Events/Ready.js
@@ -1,4 +1,4 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
 const mongoose =  require('mongoose');
 const mongoURL = process.env.mongoURL
 
@@ -8,12 +8,11 @@ module.exports = {
 	async execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 
+        client.user.setPresence({ activities: [{ name: 'shiba play', type: ActivityType.Watching }], status: 'online'});
+
         if (!mongoURL) return;
 
-        await mongoose.connect(mongoURL || "", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(mongoURL || "");
 
         if (mongoose.connect) {
             console.log("Connected to database");

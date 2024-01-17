88b72978fc91b1de703909b507628217e83cbe36
Fabi <75372147+irgendwielost@users.noreply.github.com>
2024-01-17 19:52:24 +0100

    Fixed events dir

--- src/Main.js
+++ src/Main.js
@@ -23,7 +23,7 @@ for (const folder of commandFolders) {
 	}
 }
 
-const eventsPath = path.join(__dirname, 'events');
+const eventsPath = path.join(__dirname, 'Events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
